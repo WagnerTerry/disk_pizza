8a1079df104eb8ef86d4a67117887de436b8b370
WagnerTerry <wagner.goncalves@calindra.com.br>
2021-10-31 16:23:29 -0300

    exibindo lista de refrigerantes

--- src/views/Cardapio.jsx
+++ src/views/Cardapio.jsx
@@ -5,9 +5,8 @@ import Loading from "../components/loading/Loading";
 import "./Cardapio.scss";
 
 export default function Cardapio() {
-  // const [diskPizza, setDiskPizza] = useState([]);
   const [pizzas, setPizzas] = useState([]);
-  const [refrigerantes, setRefrigerantes] = useState([]);
+  const [bebidas, setBebidas] = useState([]);
   const [grupos, setGrupos] = useState([]);
   const [loading, setLoading] = useState(true);
 
@@ -16,11 +15,13 @@ export default function Cardapio() {
     const buscaPizza = async () => {
       const getPizzas = await api.get("").then((response) => response);
       const getGrupos = await api.get("/grupos").then((response) => response);
+      const getBebidas = await api
+        .get("/bebidas")
+        .then((response) => response.data);
 
-      // setDiskPizza(response.data);
       setPizzas(getPizzas.data.pizzas);
       setGrupos(getGrupos.data.grupos);
-      // setRefrigerantes(response.data.refrigerantes);
+      setBebidas(getBebidas.bebidas);
       setLoading((oldState) => !oldState);
     };
     buscaPizza();
@@ -53,31 +54,18 @@ export default function Cardapio() {
                   {grupo.preco_grande} | Família R$ {grupo.preco_familia}{" "}
                   {grupo.preco_gigante && `| Gigante R$ ${grupo.preco_gigante}`}
                 </h4>
-
-                {/* {pizzas.map((pizza, a) => {
-                  return (
-                    <div key={a}>
-                      {pizza.grupo === grupo.grupo && <h3>{pizza.nome}</h3>}
-                    </div>
-                  );
-                })} */}
-                {/* <h4>
-                  Pequena R$ {grupo.preco_pequena} | Grande R${" "}
-                  {grupo.preco_grande} | Família R$ {grupo.preco_familia}{" "}
-                  {grupo.preco_gigante && `| Gigante R$ ${grupo.preco_gigante}`}
-                </h4> */}
               </div>
             );
           })}
 
-          {/* <strong>REFRIGERANTE GRÁTIS</strong>
-          {refrigerantes.map((refri) => {
+          <strong>REFRIGERANTE GRÁTIS</strong>
+          {bebidas.map((bebida) => {
             return (
-              <h3 key={refri.codigo_refri}>
-                Tamanho : {refri.tamanho} - {refri.litro} litros
+              <h3 key={bebida.codigo_bebida}>
+                Tamanho : {bebida.tamanho} - {bebida.litro} litros
               </h3>
             );
-          })} */}
+          })}
         </>
       )}
     </div>
