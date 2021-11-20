c855d22fdb4fb162560a8d1d9d7f769fbfe1b579
Wagner Gonçalves <wagner.goncalves@calindra.com.br>
2021-11-19 23:13:11 -0300

    adicionando sidebar ao projeto

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
 import "./App.scss";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import NavBar from './components/NavBar'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from './views/Home'
+import Caixa from './views/Caixa'
+import Cardapio from './views/Cardapio'
 
-import Routes from "./routes";
+
+//import Routes from "./routes";
 
 function App() {
+  return (
+    <>
+      <Router>
+        <NavBar />
+        <Switch>
+          <Route path="/" exact component={Home}/>
+          <Route path="/caixa" component={Caixa} />
+          <Route path="/cardapio" component={Cardapio} />
+        </Switch>
+      </Router>
+      <ToastContainer />
+    </>
+  );
+}
+
+/* function App() {
   return (
     <>
       <ToastContainer />
       <Routes />
     </>
   );
-}
+} */
 
 export default App;
