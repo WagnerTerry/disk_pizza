8802a7dbaf2638fd87168c6b7ca0e3e196ceed73
Wagner Gonçalves <wagner.goncalves@calindra.com.br>
2021-11-29 13:13:44 -0300

    icones do menu lateral

--- src/components/SidebarData.jsx
+++ src/components/SidebarData.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
-import * as FaIcons from 'react-icons/fa'
+//import * as FaIcons from 'react-icons/fa'
 import * as AiIcons from 'react-icons/ai'
-import * as IoIcons from 'react-icons/io'
+//import * as IoIcons from 'react-icons/io'
+import { MdAssignment } from "react-icons/md";
+import { MdAttachMoney } from "react-icons/md";
+import { MdRestaurantMenu } from "react-icons/md";
+
+
 
 export const SidebarData = [
     {
@@ -13,19 +18,19 @@ export const SidebarData = [
     {
         title: 'Cadastro',
         path: '/cadastro',
-        icon: <AiIcons.AiFillHome />,
+        icon: <MdAssignment />,
         cName: 'nav-text'
     },
     {
         title: 'Caixa',
         path: '/caixa',
-        icon: <IoIcons.IoIosPaper />,
+        icon: <MdAttachMoney />,
         cName: 'nav-text'
     },
     {
         title: 'Cardapio',
         path: '/cardapio',
-        icon: <FaIcons.FaCartPlus />,
+        icon: <MdRestaurantMenu />,
         cName: 'nav-text'
     },
 ]
