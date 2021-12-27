4993b2d87d51eec7322342968ad5bbe7c35826e2
Wagner Terry <goncalveswagner15@gmail.com>
2021-12-27 15:29:03 -0300

    preparando pagina de caixa

--- src/views/Caixa.jsx
+++ src/views/Caixa.jsx
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "../components/button/Button";
 import Nav from '../components/Nav'
 
 import './Caixa.scss'
 
 export default function Caixa() {
+
+  const [values, setValues] = useState([{ name: "", email: "" }])
+
+  let handleChange = (i, e) => {
+    let newFormValues = [...values]
+    newFormValues[i][e.target.value] = e.target.value
+    setValues(newFormValues)
+  }
+
+  let addFormFields = () => {
+    setValues([...values, { name: "", email: "" }])
+  }
+
+  let removeFormFields = (i) => {
+    let newFormValues = [...values]
+    newFormValues.splice(i, 1)
+    setValues(newFormValues)
+  }
+
+  let handleSubmit = (e) => {
+    e.preventDefault()
+    alert(JSON.stringify(values))
+  }
+
   return (
     <div id="caixa">
       <div className="component-nav">
@@ -12,7 +36,26 @@ export default function Caixa() {
       </div>
       <h2>Caixa</h2>
       <div className="cash-flow">
-        <Button onClick={() => console.log("foi")} color="turquoise">Nova Entrada</Button>
+        <Button onClick={() => addFormFields()} color="turquoise">Abrir Caixa</Button>
+
+        {/* Pedido, Data, Hora, Cliente, Bairro, Entregador, Situação, Valor total */}
+
+        {values.map((element, index) => (
+          <div className="form-inline" key={index}>
+            <label>Name</label>
+            <input type="text" name="name" value={element.name || ""} onChange={(e) => handleChange(index, e)} />
+            <label>Email</label>
+            <input type="text" name="email" value={element.email || ""} onChange={e => handleChange(index, e)} />
+
+            <button type="button" className="button remove" onClick={() => removeFormFields(index)}>Remover</button>
+
+          </div>
+        ))}
+        <div className="button-section">
+          {/* <button className="button add" type="button" onClick={() => addFormFields()}>Add</button> */}
+          <button className="button submit" type="submit" onClick={handleSubmit}>Submit</button>
+        </div>
+
       </div>
     </div>
   )
